145280cb06712a767e9611065cfd3aaff0f3ba10
Jose Folgar <f.jose@wustl.edu>
2024-02-18 20:18:27 -0600

    Added stuff from Q's we might want to use in the future

--- KaleidoScoped/Assets/Code/Enemy.cs
+++ KaleidoScoped/Assets/Code/Enemy.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Outlets
+    //NavMeshAgent navAgent;
+    Animator animator;
+
+    // Configuration
+    public Transform target;
+    //public Transform patrolRoute;
+
+    // State Tracking
+    //int patrolIndex;
+
+    // Methods
     void Start()
     {
-        
+        //navAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target)
+        {
+            //navAgent.SetDestination(target.position);
+        }
+
+        //animator.SetFloat("velocity", navAgent.velocity.magnitude);
     }
 }
