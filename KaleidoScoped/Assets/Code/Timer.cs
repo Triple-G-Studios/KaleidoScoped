7aa78fe7a3119cdf4dc4e9e7852dd32f2b36bb11
Luke Hermann <hermannl@wustl.edu>
2024-02-21 21:12:46 -0600

    Added timer

---
+++ KaleidoScoped/Assets/Code/Timer.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float currTime = 0f;
+    public float startTime = 360f;
+
+    [SerializeField] Text timerText;
+
+    void Start()
+    {
+        currTime = startTime;
+    }
+
+    void Update()
+    {
+        currTime -= 1 * Time.deltaTime;
+        var ts = TimeSpan.FromSeconds(currTime);
+        timerText.text = string.Format("{0:00}:{1:00}", (int) ts.TotalMinutes, (int) ts.Seconds);
+
+        if(currTime < 60)
+        {
+            timerText.color = Color.red;
+        }
+
+        if (currTime < 0)
+        {
+            currTime = 0;
+        }
+    }
+}
