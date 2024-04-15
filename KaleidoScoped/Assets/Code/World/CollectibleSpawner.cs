a70586d66ac5ceeb5cb30b9c9d4f9790eb2c4b4a
JKoonce2002 <71308122+JKoonce2002@users.noreply.github.com>
2024-04-15 01:47:25 -0500

    Better Orbs

--- KaleidoScoped/Assets/Code/World/CollectibleSpawner.cs
+++ KaleidoScoped/Assets/Code/World/CollectibleSpawner.cs
@@ -13,16 +13,37 @@ namespace Kaleidoscoped
         //State Tracking
         public int spawnIndex = 0;
         public int prefabIndex = 0;
-        public GameObject currentCollectible;
+        public GameObject currentCollectible1, currentCollectible2, currentCollectible3;
+        public int index1 = -1, index2 = -1, index3 = -1;
 
         // Update is called once per frame
         void Update()
         {
             prefabIndex = Random.Range(0, prefabs.Length);
             spawnIndex = Random.Range(0, spawnPoints.Length);
+            bool thisOne = false;
 
-            if (currentCollectible == null) { 
-                currentCollectible = Instantiate(prefabs[prefabIndex], spawnPoints[spawnIndex].transform.position, Quaternion.identity);
+
+            if (!(spawnIndex == index1 || spawnIndex == index2 || spawnIndex == index3))
+            {
+                if (currentCollectible1 == null && !thisOne)
+                {
+                    thisOne = true;
+                    index1 = spawnIndex;
+                    currentCollectible1 = Instantiate(prefabs[prefabIndex], spawnPoints[spawnIndex].transform.position, Quaternion.identity);
+                }
+                if (currentCollectible2 == null && !thisOne)
+                {
+                    thisOne = true;
+                    index2 = spawnIndex;
+                    currentCollectible2 = Instantiate(prefabs[prefabIndex], spawnPoints[spawnIndex].transform.position, Quaternion.identity);
+                }
+                if (currentCollectible3 == null && !thisOne)
+                {
+                    thisOne = true;
+                    index3 = spawnIndex;
+                    currentCollectible3 = Instantiate(prefabs[prefabIndex], spawnPoints[spawnIndex].transform.position, Quaternion.identity);
+                }
             }
         }
     }
